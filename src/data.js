5ea3d830b7b5e270a68eb417766a6734aa1da992
henry <etawara@itba.edu.ar>
2020-08-27 17:36:43 -0300

    release: new viz 2020W34

--- src/data.js
+++ src/data.js
@@ -1,16 +1,22 @@
 export default {
     VIZ_LIST: [
         {
-            title:  'InfoVis 2020/W33: The state of the Digital Nation',
-            name:   'MM 2020 - Week 33',
-            path:   '2020W33',
-            url:    'https://public.tableau.com/views/InfoVis2020W33ThestateoftheDigitalNation/Dashboard1?:language=en&:display_count=y&:origin=viz_share_link'
+            title: 'InfoVis 2020/W34: Sexual and Reproductive Health and Rights (SRHR)',
+            path: '2020W34',
+            name: 'MM 2020 - Week 34',
+            url: 'https://public.tableau.com/shared/GY35FTRNS?:display_count=y&:origin=viz_share_link'
         },
         {
-            title:  'InfoVis 2020/W32: Benefits of Remote Work',
-            path:   '2020W32',
-            name:   'MM 2020 - Week 32',
-            url:    'https://public.tableau.com/views/InfoVis2020W32BenefitsofRemoteWork/Dashboard1?:language=en&:display_count=y&:origin=viz_share_link'
+            title: 'InfoVis 2020/W33: The state of the Digital Nation',
+            path: '2020W33',
+            name: 'MM 2020 - Week 33',
+            url: 'https://public.tableau.com/views/InfoVis2020W33ThestateoftheDigitalNation/Dashboard1?:language=en&:display_count=y&:origin=viz_share_link'
+        },
+        {
+            title: 'InfoVis 2020/W32: Benefits of Remote Work',
+            path: '2020W32',
+            name: 'MM 2020 - Week 32',
+            url: 'https://public.tableau.com/views/InfoVis2020W32BenefitsofRemoteWork/Dashboard1?:language=en&:display_count=y&:origin=viz_share_link'
         }
-      ],
- }
+    ],
+}
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,33 +1,26 @@
 <template>
   <div class="home">
-    <h1>This is a home page</h1>
+    <h1>Welcome to infovis</h1>
     <h2>Visualizations</h2>
     <v-container fluid>
       <v-row align="start" justify="start" style="height: 300px;">
         <v-col cols="3" v-for="card in getVizList()" :key="card.path">
           <v-card :to="'/viz/'.concat(card.path)" width="300px" class="ma-3" outlined tile>
+            <v-img
+              :src="require(`../assets/${card.path}.png`)"
+              aspect-ratio="1"
+              class="grey lighten-2"
+              max-width="300"
+              max-height="200"
+            ></v-img>
 
-          <v-img
-      :src="require(`../assets/${card.path}.png`)"
-      aspect-ratio="1"
-      class="grey lighten-2"
-      max-width="300"
-      max-height="200"
-    >
-      
-    </v-img>
+            <template v-slot:placeholder>
+              <v-row class="fill-height ma-0" align="center" justify="center">
+                <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
+              </v-row>
+            </template>
 
-    <template v-slot:placeholder>
-        <v-row
-          class="fill-height ma-0"
-          align="center"
-          justify="center"
-        >
-          <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
-        </v-row>
-      </template>
-
-          <v-card-title v-text="card.name"></v-card-title>
+            <v-card-title v-text="card.name"></v-card-title>
           </v-card>
         </v-col>
       </v-row>
